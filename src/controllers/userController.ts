58ca224e81c404ea402b2b8c1ef42f39052dd2fb
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-05-11 15:02:25 +0600

    feat: implement controllers for each CRUD operation

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,11 +3,12 @@ import { getRequestBody } from '../utils/getRequestBody';
 import { UserPayload } from '../types/user.types';
 import {
   createUser,
-  // getUserById,
-  // getUsers,
-  // updateUser,
-  // deleteUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
 } from '../models/user';
+import { validate } from 'uuid';
 import { isValidUserPayload } from '../utils/validators/userPayloadValidator';
 
 export const createUserController = async (
@@ -26,8 +27,98 @@ export const createUserController = async (
     const newUser = createUser(username, age, hobbies);
     res.writeHead(201, { 'Content-type': 'application/json' });
     res.end(JSON.stringify(newUser));
+    return;
   } catch (error) {
     res.writeHead(400, { 'Content-type': 'application/json' });
     res.end(JSON.stringify({ message: (error as Error).message }));
+    return;
   }
+};
+
+export const getUsersController = (
+  _req: IncomingMessage,
+  res: ServerResponse
+) => {
+  const users = getUsers();
+  res.writeHead(200, { 'Content-type': 'application/json' });
+  res.end(JSON.stringify(users));
+  return;
+};
+
+export const getUserByIdController = (
+  _req: IncomingMessage,
+  res: ServerResponse,
+  id: string
+) => {
+  if (!validate(id)) {
+    res.writeHead(400, { 'Content-type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Invalid user ID' }));
+    return;
+  }
+
+  const user = getUserById(id);
+  if (!user) {
+    res.writeHead(404, { 'Content-type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User not found' }));
+    return;
+  }
+
+  res.writeHead(200, { 'Content-type': 'application/json' });
+  res.end(JSON.stringify(user));
+  return;
+};
+
+export const updateUserController = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+  id: string
+) => {
+  if (!validate(id)) {
+    res.writeHead(400, { 'Content-type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Invalid user ID' }));
+    return;
+  }
+
+  try {
+    const payload = await getRequestBody<Partial<UserPayload>>(req);
+    const updatedUser = updateUser(id, payload);
+
+    if (!updatedUser) {
+      res.writeHead(404, { 'Content-type': 'application/json' });
+      res.end(JSON.stringify({ message: 'User not found' }));
+      return;
+    }
+
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(JSON.stringify(updatedUser));
+    return;
+  } catch (error) {
+    res.writeHead(400, { 'Content-type': 'application/json' });
+    res.end(JSON.stringify({ message: (error as Error).message }));
+    return;
+  }
+};
+
+export const deleteUserController = (
+  _req: IncomingMessage,
+  res: ServerResponse,
+  id: string
+) => {
+  if (!validate(id)) {
+    res.writeHead(400, { 'Content-type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Invalid user ID' }));
+    return;
+  }
+
+  const isDeleted = deleteUser(id);
+
+  if (!isDeleted) {
+    res.writeHead(404, { 'Content-type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User not found' }));
+    return;
+  }
+
+  res.writeHead(204, { 'Content-type': 'application/json' });
+  res.end();
+  return;
 };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,12 +1,45 @@
 import { IncomingMessage, ServerResponse } from 'node:http';
-import { createUserController } from '../controllers/userController';
+import {
+  createUserController,
+  deleteUserController,
+  getUserByIdController,
+  getUsersController,
+  updateUserController,
+} from '../controllers/userController';
 import { HttpMethods } from '../types/http-methods';
 
 export const userRouter = (req: IncomingMessage, res: ServerResponse) => {
   const method = req.method;
   const url = req.url;
 
-  if (method === HttpMethods.POST && url === '/api/users') {
-    createUserController(req, res);
+  if (!url) return;
+  const [base, resource, id] = url.split('/').slice(1);
+
+  if (method === HttpMethods.POST && `/${base}/${resource}` === '/api/users') {
+    return createUserController(req, res);
+  }
+
+  if (method === HttpMethods.GET && `/${base}/${resource}` === '/api/users') {
+    if (id) {
+      return getUserByIdController(req, res, id);
+    }
+
+    return getUsersController(req, res);
+  }
+
+  if (
+    method === HttpMethods.PUT &&
+    `/${base}/${resource}` === '/api/users' &&
+    id
+  ) {
+    return updateUserController(req, res, id);
+  }
+
+  if (
+    method === HttpMethods.DELETE &&
+    `/${base}/${resource}` === '/api/users' &&
+    id
+  ) {
+    return deleteUserController(req, res, id);
   }
 };
